69d5fb31ca08a141a303c3b5d62fb0c33bcc332f
Nadim Neto <70706193+NadimNeto@users.noreply.github.com>
2022-08-01 15:14:01 -0300

    Refatoramento: função p/ alterar msg de dica

--- script.js
+++ script.js
@@ -7,16 +7,21 @@ let score = 100;
 //Variável para a Pontuação Máxima
 let highscore = 0;
 
+//função para alterar a mensagem de dica.
+const alteraMesg = function (mensagem) {
+  document.querySelector(".message").textContent = mensagem;
+};
+
 //Evento ao clicar no botão "Chutar!".
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
     //Nenhum número chutado.
-    document.querySelector(".message").textContent = "⛔ Sem Número!";
+    alteraMesg("⛔ Sem Número!");
   } else if (guess === secretNumber) {
     //Número chutado corretamente.
-    document.querySelector(".message").textContent = "🎉 Número Correto!";
+    alteraMesg("🎉 Número Correto!");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
@@ -28,13 +33,14 @@ document.querySelector(".check").addEventListener("click", function () {
   } else {
     if (score > 1) {
       //Verifica se perdeu ou não.
-      document.querySelector(".message").textContent =
-        guess > secretNumber ? "📈 Muito alto..." : "📉 Muito baixo..."; //Ternário para definir o contúdo escrito da dica.
+      alteraMesg(
+        guess > secretNumber ? "📈 Muito alto..." : "📉 Muito baixo..."
+      ); //Ternário para definir o contúdo escrito da dica.
       score--;
       document.querySelector(".score").textContent = score;
     } else {
       //Ao perder.
-      document.querySelector(".message").textContent = "💥 Você perdeu! 🙁";
+      alteraMesg("💥 Você perdeu! 🙁");
       document.querySelector(".score").textContent = 0;
     }
   }
@@ -44,7 +50,7 @@ document.querySelector(".check").addEventListener("click", function () {
 document.querySelector(".again").addEventListener("click", function () {
   score = 100;
   secretNumber = Math.trunc(Math.random() * 100) + 1;
-  document.querySelector(".message").textContent = "Chute um valor...";
+  alteraMesg("Chute um valor...");
   document.querySelector(".number").textContent = "?";
   document.querySelector(".score").textContent = score;
   document.querySelector(".guess").value = "";
